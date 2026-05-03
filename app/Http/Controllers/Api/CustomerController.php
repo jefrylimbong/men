0841c609f0137e187217f8d7acd2e20833a5bf05
Jefry <jefrylimbong@gmail.com>
2026-05-03 18:12:09 +0800

    Big Update

--- app/Http/Controllers/Api/CustomerController.php
+++ app/Http/Controllers/Api/CustomerController.php
@@ -43,116 +43,42 @@ class CustomerController extends Controller
             });
         }
 
-        $customers = collect();
+        $assignedFinanceIds = $user ? $user->financeMasters()->pluck('finance_masters.id') : collect();
 
-        if ($user && $user->type === 'user') {
-            $assignedFinanceIds = $user->financeMasters()->pluck('finance_masters.id');
-
-            if ($assignedFinanceIds->isNotEmpty()) {
-                if ($isSync) {
-                    $branchIds = FinanceBranch::whereIn('finance_master_id', $assignedFinanceIds)->pluck('id');
-                    $lastId = $request->input('last_id', 0);
-
-                    $customers = CustomerData::query()
-                        ->join('finance_branches', 'customer_data.finance_branch_id', '=', 'finance_branches.id')
-                        ->join('finance_masters', 'finance_branches.finance_master_id', '=', 'finance_masters.id')
-                        ->join('location_masters', 'finance_branches.location_master_id', '=', 'location_masters.id')
-                        ->select(
-                            'customer_data.id',
-                            'customer_data.nopol',
-                            'customer_data.nama',
-                            'customer_data.norak',
-                            'customer_data.nosin',
-                            'customer_data.tipe',
-                            'finance_masters.fin_name as finance_name',
-                            'location_masters.name as location_name'
-                        )
-                        ->where('customer_data.is_active', true)
-                        ->whereIn('customer_data.finance_branch_id', $branchIds)
-                        ->where('customer_data.id', '>', $lastId)
-                        ->oldest('customer_data.id')
-                        ->simplePaginate(2000);
-                } else {
-                    // Pencarian Online Teroptimasi: Satu Query Langsung
-                    $customers = $baseQuery
-                        ->whereHas('financeBranch', function ($q) use ($assignedFinanceIds) {
-                            $q->whereIn('finance_master_id', $assignedFinanceIds);
-                        })
-                        ->latest()
-                        ->limit(10) // Tingkatkan limit ke 10 agar lebih berguna
-                        ->get();
-                }
-            }
-
-        } elseif ($user && $user->type !== 'superadmin') {
-            // Tipe selain superadmin & user (misal admin)
-            $assignedFinanceIds = $user->financeMasters()->pluck('finance_masters.id');
-            $baseQuery->whereHas('financeBranch', function ($q) use ($assignedFinanceIds) {
-                $q->whereIn('finance_master_id', $assignedFinanceIds);
-            });
-
-            $baseQuery->latest();
-
-            if (! $isSync) {
-                $baseQuery->limit(5);
-                $customers = $baseQuery->get();
-            } else {
-                $lastId = $request->input('last_id', 0);
-                $customers = CustomerData::query()
-                    ->join('finance_branches', 'customer_data.finance_branch_id', '=', 'finance_branches.id')
-                    ->join('finance_masters', 'finance_branches.finance_master_id', '=', 'finance_masters.id')
-                    ->join('location_masters', 'finance_branches.location_master_id', '=', 'location_masters.id')
-                    ->select(
-                        'customer_data.id',
-                        'customer_data.nopol',
-                        'customer_data.nama',
-                        'customer_data.norak',
-                        'customer_data.nosin',
-                        'customer_data.tipe',
-                        'finance_masters.fin_name as finance_name',
-                        'location_masters.name as location_name'
-                    )
-                    ->where('customer_data.is_active', true)
-
-                    /**
-                     * FILTER CABANG (Toggle)
-                     * Hilangkan komentar line di bawah jika ingin membatasi data berdasarkan cabang yang diassign ke admin.
-                     * Berikan komentar (//) jika ingin menarik SEMUA data aktif ke HP.
-                     */
-                    ->whereIn('customer_data.finance_branch_id', FinanceBranch::whereIn('finance_master_id', $assignedFinanceIds)->pluck('id'))
-
-                    ->where('customer_data.id', '>', $lastId)
-                    ->oldest('customer_data.id')
-                    ->simplePaginate(2000);
-            }
-
+        if (! $isSync) {
+            // Pencarian Online: Buka akses untuk semua data aktif tanpa filter cabang
+            $customers = $baseQuery
+                ->latest()
+                ->limit(10)
+                ->get();
         } else {
-            // Superadmin
-            $baseQuery->latest();
-            if (! $isSync) {
-                $baseQuery->limit(5);
-                $customers = $baseQuery->get();
+            // Sinkronisasi: Tetap gunakan filter cabang (kecuali superadmin)
+            if ($user && $user->type === 'superadmin') {
+                $query = CustomerData::query();
             } else {
-                $lastId = $request->input('last_id', 0);
-                $customers = CustomerData::query()
-                    ->join('finance_branches', 'customer_data.finance_branch_id', '=', 'finance_branches.id')
-                    ->join('finance_masters', 'finance_branches.finance_master_id', '=', 'finance_masters.id')
-                    ->join('location_masters', 'finance_branches.location_master_id', '=', 'location_masters.id')
-                    ->select(
-                        'customer_data.id',
-                        'customer_data.nopol',
-                        'customer_data.nama',
-                        'customer_data.norak',
-                        'customer_data.nosin',
-                        'customer_data.tipe',
-                        'finance_masters.fin_name as finance_name',
-                        'location_masters.name as location_name'
-                    )
-                    ->where('customer_data.is_active', true)
-                    ->where('customer_data.id', '>', $lastId)
-                    ->oldest('customer_data.id')
-                    ->simplePaginate(2000);
+                $branchIds = FinanceBranch::whereIn('finance_master_id', $assignedFinanceIds)->pluck('id');
+                $query = CustomerData::whereIn('finance_branch_id', $branchIds);
             }
+
+            $lastId = $request->input('last_id', 0);
+            $customers = $query
+                ->join('finance_branches', 'customer_data.finance_branch_id', '=', 'finance_branches.id')
+                ->join('finance_masters', 'finance_branches.finance_master_id', '=', 'finance_masters.id')
+                ->join('location_masters', 'finance_branches.location_master_id', '=', 'location_masters.id')
+                ->select(
+                    'customer_data.id',
+                    'customer_data.nopol',
+                    'customer_data.nama',
+                    'customer_data.norak',
+                    'customer_data.nosin',
+                    'customer_data.tipe',
+                    'finance_masters.fin_name as finance_name',
+                    'location_masters.name as location_name'
+                )
+                ->where('customer_data.is_active', true)
+                ->where('customer_data.id', '>', $lastId)
+                ->oldest('customer_data.id')
+                ->simplePaginate(2000);
         }
 
         // Jika response bukan paginator (untuk online search), buat array structure agar tetap kompatibel
@@ -162,7 +88,7 @@ class CustomerController extends Controller
 
         return response()->json([
             'status' => 'success',
-            'assigned_finances' => $assignedFinanceIds ?? [],
+            'assigned_finances' => $assignedFinanceIds,
             'data' => $responseData,
         ]);
     }
