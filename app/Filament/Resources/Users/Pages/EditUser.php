56af3a24c77a4a5421b1511ac46f87dec4cdcba3
Jefry <jefrylimbong@gmail.com>
2026-05-04 20:20:12 +0800

    Edit user Form

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -3,11 +3,8 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
-use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
-use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
-use Illuminate\Support\Facades\Hash;
 
 class EditUser extends EditRecord
 {
@@ -16,24 +13,6 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Action::make('resetPassword')
-                ->label('Reset Password')
-                ->color('warning')
-                ->icon('heroicon-m-key')
-                ->requiresConfirmation()
-                ->modalHeading('Reset Password')
-                ->modalDescription('Apakah Anda yakin ingin mereset password user ini menjadi 123456?')
-                ->action(function ($record) {
-                    $record->update([
-                        'password' => Hash::make('123456'),
-                    ]);
-
-                    Notification::make()
-                        ->success()
-                        ->title('Berhasil')
-                        ->body('Password berhasil direset menjadi 123456.')
-                        ->send();
-                }),
             DeleteAction::make(),
         ];
     }
