6e6d501cfc2238c72006471c3077d2b2a87a8cb5
Jefry <jefrylimbong@gmail.com>
2026-05-04 21:17:10 +0800

    Edit user Form

--- database/migrations/2026_05_03_184715_add_indexes_to_customer_data_table.php
+++ database/migrations/2026_05_03_184715_add_indexes_to_customer_data_table.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Migration untuk menambah index pada kolom pencarian customer_data
  * Jalankan: php artisan migrate
@@ -13,31 +14,62 @@ return new class extends Migration
     {
         // Hapus indeks jika sudah ada (manual via SQL agar aman)
         try {
-            \DB::statement('ALTER TABLE customer_data DROP INDEX customer_search_index');
-        } catch (\Exception $e) {}
+            DB::statement('ALTER TABLE customer_data DROP INDEX customer_search_index');
+        } catch (Exception $e) {
+        }
 
         try {
-            \DB::statement('ALTER TABLE customer_data DROP INDEX customer_data_nopol_fulltext');
-        } catch (\Exception $e) {}
+            DB::statement('ALTER TABLE customer_data DROP INDEX customer_data_nopol_fulltext');
+        } catch (Exception $e) {
+        }
 
         Schema::table('customer_data', function (Blueprint $table) {
-            // Buat ulang semua indeks
-            $table->fullText('nopol');
-            $table->fullText('nama');
-            $table->fullText('norak');
-            $table->fullText('nosin');
-            $table->fullText(['nopol', 'nama', 'norak', 'nosin'], 'customer_search_index');
+            try {
+                $table->fullText('nopol');
+            } catch (Exception $e) {
+            }
+            try {
+                $table->fullText('nama');
+            } catch (Exception $e) {
+            }
+            try {
+                $table->fullText('norak');
+            } catch (Exception $e) {
+            }
+            try {
+                $table->fullText('nosin');
+            } catch (Exception $e) {
+            }
+            try {
+                $table->fullText(['nopol', 'nama', 'norak', 'nosin'], 'customer_search_index');
+            } catch (Exception $e) {
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('customer_data', function (Blueprint $table) {
-            $table->dropFullText(['nopol']);
-            $table->dropFullText(['nama']);
-            $table->dropFullText(['norak']);
-            $table->dropFullText(['nosin']);
-            $table->dropFullText('customer_search_index');
+            try {
+                $table->dropFullText(['nopol']);
+            } catch (Exception $e) {
+            }
+            try {
+                $table->dropFullText(['nama']);
+            } catch (Exception $e) {
+            }
+            try {
+                $table->dropFullText(['norak']);
+            } catch (Exception $e) {
+            }
+            try {
+                $table->dropFullText(['nosin']);
+            } catch (Exception $e) {
+            }
+            try {
+                $table->dropFullText('customer_search_index');
+            } catch (Exception $e) {
+            }
         });
     }
 };
