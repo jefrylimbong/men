261e9ca8e89698a5b5e511df172963ca35224e3f
Jefry <jefrylimbong@gmail.com>
2026-05-15 00:36:45 +0800

    Add scout

--- app/Models/CustomerData.php
+++ app/Models/CustomerData.php
@@ -47,9 +47,9 @@ class CustomerData extends Model
         // Deteksi apakah Nopol ini "asli" atau cuma Nomor Mesin (biasanya mesin ada tanda '-')
         $isRealNopol = (!str_contains($this->nopol, '-')) && (strlen($this->nopol) < 10);
 
-        // Pintasan: Pisahkan huruf dan angka dengan spasi agar mudah dicari tengahnya
-        // Contoh: DB2148MJ menjadi DB 2148 MJ
-        $formattedNopol = preg_replace('/([A-Z]+)([0-9]+)([A-Z]+)/i', '$1 $2 $3', $this->nopol);
+        // Pemisah yang lebih fleksibel: Pisahkan setiap grup huruf dan angka
+        // DB2148MJ -> DB 2148 MJ | B123 -> B 123
+        $formattedNopol = trim(preg_replace('/([A-Z]+|[0-9]+)/i', '$1 ', $this->nopol));
 
         return [
             'id' => (int) $this->id,
