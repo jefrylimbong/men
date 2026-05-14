737681ab294ff8b346d32cad10b23cd85ba43acc
Jefry <jefrylimbong@gmail.com>
2026-05-14 23:57:55 +0800

    Add scout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,6 +41,7 @@ class AppServiceProvider extends ServiceProvider
             try {
                 $client = new \Meilisearch\Client(config('scout.meilisearch.host'), config('scout.meilisearch.key'));
                 $client->index('customer_data')->updateFilterableAttributes(['is_active']);
+                $client->index('customer_data')->updateSearchableAttributes(['nopol']);
             } catch (\Exception $e) {
                 // Silently fail if Meilisearch is not reachable
             }
