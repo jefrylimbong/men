0f6ddb05606544896e6e3ac43f86507de4a7a3ff
Jefry <jefrylimbong@gmail.com>
2026-05-15 00:35:07 +0800

    Add scout

--- app/Models/CustomerData.php
+++ app/Models/CustomerData.php
@@ -47,13 +47,17 @@ class CustomerData extends Model
         // Deteksi apakah Nopol ini "asli" atau cuma Nomor Mesin (biasanya mesin ada tanda '-')
         $isRealNopol = (!str_contains($this->nopol, '-')) && (strlen($this->nopol) < 10);
 
+        // Pintasan: Pisahkan huruf dan angka dengan spasi agar mudah dicari tengahnya
+        // Contoh: DB2148MJ menjadi DB 2148 MJ
+        $formattedNopol = preg_replace('/([A-Z]+)([0-9]+)([A-Z]+)/i', '$1 $2 $3', $this->nopol);
+
         return [
             'id' => (int) $this->id,
-            'nopol' => $this->nopol,
+            'nopol' => $formattedNopol, // Meilisearch akan indeks dengan spasi
             'nama' => $this->nama,
             'tipe' => $this->tipe,
             'is_active' => (int) $this->is_active,
-            'is_real_nopol' => $isRealNopol ? 1 : 0, // Kolom pembantu
+            'is_real_nopol' => $isRealNopol ? 1 : 0,
         ];
     }
 }
